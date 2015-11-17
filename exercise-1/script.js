acdc6cdf0c53756756830e2d128c7581784c6876
Sara <saraclefort@gmail.com>
2015-11-16 21:10:19 -0800

    flashing todo list

--- exercise-1/script.js
+++ exercise-1/script.js
@@ -1,5 +1,15 @@
-/*
 
-  Your Javascript here
 
-*/
+function makeList () {
+	var item = document.getElementById("item");
+	var toDoList = document.getElementById("todolist");
+
+	var li = document.createElement("li");
+	var listContent = document.createTextNode(item.value);
+
+	li.appendChild(listContent);
+	toDoList.appendChild(li);
+
+};
+
+document.addEventListener("submit", makeList);
